a8f5417831fc0753af0030717af05c8877f50a18
Felix Cary Giovanni <felix.cary@gmail.com>
2021-05-28 12:20:33 +0700

    adding siklus logo and siklus color pallete

---
+++ lib/Extension/ColorsExtension.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HexColor extends Color {
+  static int _getColorFromHex(String hexColor) {
+    hexColor = hexColor.toUpperCase().replaceAll("#", "");
+    if (hexColor.length == 6) {
+      hexColor = "FF" + hexColor;
+    }
+    return int.parse(hexColor, radix: 16);
+  }
+
+  HexColor(final String hexColor) : super(_getColorFromHex(hexColor));
+}
+
+extension SiklusColorPallete on Color {
+  static get blueCyan => HexColor("#58C8DC");
+}
